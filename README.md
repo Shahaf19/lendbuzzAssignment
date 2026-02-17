c595924aa9277261fd7ac2635825d64d4ebbf27c
Shahaf Cohen <Shahaf1@post.bgu.ac.il>
2026-02-17 20:35:06 +0200

    upset completed

--- README.md
+++ README.md
@@ -10,12 +10,16 @@ This project implements a local data platform using Python and DuckDB for data i
 ```bash
 python scripts/ingest.py
 ```
-
+### 1.b. Upsert
+Refactored the script to accept a CSV path as argument and support upsert logic (using VIN as the unique key).
+```bash
+python scripts/ingest.py data/raw/cars_data_2.csv
+```
 ## Time Log
 | Task | Time Spent |
 |------|------------|
 | 1.a. Initialization | 2 hours |
-| 1.b. Upsert | |
+| 1.b. Upsert | 45 min |
 | 1.c. Schema Change | |
 | 2.a. dbt Setup | |
 | 2.b. Data Modeling | |
--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,5 +1,43 @@
+import argparse
 import duckdb
+import os
 
-con = duckdb.connect('data/db/my_db.duckdb')
-con.sql("CREATE TABLE car_inventory AS SELECT * FROM read_csv_auto('data/raw/cars_data_1.csv')")
-con.close()
+DB_PATH = 'data/db/Cars_Inventory.duckdb'
+TABLE_NAME = "car_inventory"
+
+def ingest_csv(con, csv_path, table_name):
+    """Upsert CSV data into DuckDB table using VIN as the primary key."""
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError(f"CSV file not found: {csv_path}")
+    
+    # Check if the table exists
+    table_exists = con.sql(
+        f"SELECT count(*) FROM information_schema.tables WHERE table_name = '{table_name}'"
+        ).fetchone()[0] > 0
+
+    if not table_exists:
+        con.sql(f"CREATE TABLE {table_name} AS SELECT * FROM read_csv_auto('{csv_path}')")
+
+    # Deleting the potentially outdated records and replace them with recentcd
+    else:
+        con.sql(f"CREATE TEMP TABLE staging AS SELECT * FROM read_csv_auto('{csv_path}')")
+        con.sql(f"DELETE FROM {table_name} WHERE vin IN (SELECT vin FROM staging)")
+        con.sql(f"INSERT INTO {table_name} SELECT * FROM staging")
+        con.sql("DROP TABLE staging")
+
+    print(f"Ingested {csv_path} into '{table_name}'")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csv_path")
+    args = parser.parse_args()
+
+    con = duckdb.connect(DB_PATH)
+    try:
+        ingest_csv(con, args.csv_path, TABLE_NAME)
+    finally:
+        con.close()
+
+if __name__ == "__main__":
+    main()
